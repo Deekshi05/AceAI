9c50dcc04aa147bb10b8fbd8cfd294bdd8478e29
Deekshi05 <deekshithavemula50@gmail.com>
2025-08-28 22:46:34 +0530

    feat(jobdescription):added feature to enter or paste the jobdescription

--- app/(routes)/_components/JobDescription.jsx
+++ app/(routes)/_components/JobDescription.jsx
@@ -1,9 +1,17 @@
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
-
-function JobDescription() {
+function JobDescription({onHandleInputChange}) {
   return (
-    <div>
-      
+    <div >
+      <div >
+        <label>Job Title</label>
+          <Input placeholder="Full Stack Developer" onChange={(e)=>onHandleInputChange('jobTitle',e.target.value)}></Input>
+      </div>
+      <div className='mt-6'>
+        <label>Job Description</label>
+        <Textarea placeholder="Enter or paste Job Description" className='h-[200px]' onChange={(e)=>onHandleInputChange('jobDescription',e.target.value)} ></Textarea>
+      </div>
     </div>
   )
 }
